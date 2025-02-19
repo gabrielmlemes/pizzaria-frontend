58a171500ca2c65e2f997e3bb12586b9a6cc561f
Gabriel Lemes <g.moreiralemess@gmail.com>
2025-02-19 15:02:03 -0300

    feat: add login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,46 @@
 import Image from "next/image";
 import logoImg from "/public/pizza-svgrepo-com.svg";
+import { api } from "@/lib/api";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+  async function handleLogin(formData: FormData) {
+    "use server";
+
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    if (email === "" || password === "") {
+      return;
+    }
+
+    try {
+      const response = await api.post("/session", {
+        email,
+        password,
+      });
+
+      if (!response.data.token) {
+        return;
+      }
+
+      // Guardar o token do usuário dentro de um cookie
+      const expressTime = 60 * 60 * 24 * 30 * 1000; // 30d em milisegundos
+      const cookiesStore = await cookies();
+
+      cookiesStore.set("session", response.data.token, {
+        maxAge: expressTime,
+        httpOnly: false,
+        path: "/",
+        secure: process.env.NODE_END === "production",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+    redirect("/dashboard");
+  }
+
   return (
     <div className="w-full min-h-screen flex justify-center items-center flex-col">
       <div className="flex items-center justify-center w-full px-2">
@@ -13,7 +52,7 @@ export default function Home() {
         </div>
       </div>
 
-      <form action="" className="w-full max-w-96 pt-4">
+      <form action={handleLogin} className="w-full max-w-96 pt-4">
         <div className="flex flex-col items-center justify-center gap-4">
           <input
             type="email"
@@ -24,9 +63,10 @@ export default function Home() {
           />
           <input
             type="password"
-            placeholder="Digite sua senha"
+            placeholder="*********"
             className="px-3 h-11 rounded-md bg-[var(--dark-900)] border border-slate-600 text-gray-50 w-full"
             required
+            name="password"
           />
           <button
             className="bg-red-700 w-full hover:scale-105 duration-300 text-white font-semibold h-11 rounded-md hover:scale-h-115"
